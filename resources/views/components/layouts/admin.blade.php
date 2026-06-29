02a4794df728f510d4359fbf8266b873f3068121
Saman <mohamedshaman003@gmail.com>
2026-06-29 20:27:11 +0530

    icon

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title ?? 'Page Title' }}</title>
-    <link rel="icon" type="image/png" href="{{ asset('images/hardmenicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
